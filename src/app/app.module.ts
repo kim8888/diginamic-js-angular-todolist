ae302f8f1af3c4817331f6a7e0be41f033f87abb
kim8888 <mazhoudkarim88@gmail.com>
2019-10-27 16:49:29 +0100

    TP Todolist Pour Valentin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoEditComponent } from './todo-edit/todo-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TodoListComponent,
+    TodoEditComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/model/todo.model.ts
@@ -0,0 +1,9 @@
+  
+export class Todo{
+
+    constructor(public id: number,
+                public label: string){
+                    
+                }
+}
+export type Todos = Array<Todo>;
